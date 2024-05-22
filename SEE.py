9e5f4abf96915877d88b038cc114e78bfdf863fb
blupho <82224096+blupho@users.noreply.github.com>
2024-05-22 18:59:13 -0500

    Update SEE.py

--- SEE.py
+++ SEE.py
@@ -10,22 +10,22 @@ st.set_page_config(
 ############ CREATE THE LOGO AND HEADING ############
 
 # We create a set of columns to display the logo and the heading next to each other.
-# c1, c2 = st.columns([0.32, 2])
-# The snowflake logo will be displayed in the first column, on the left.
+ c1, c2 = st.columns([0.32, 2])
+ The snowflake logo will be displayed in the first column, on the left.
 
-# with c1:
+with c1:
 
-    # st.image(
-#         "images/logo.png",
-#         width=85,
-#     )
+    st.image(
+       "https://response.restoration.noaa.gov/sites/default/files/skimming_dwh_2010_noaa_520.jpg",
+         width=180,
+     )
 
 
 # # The heading will be on the right.
 
-# with c2:
-#     st.caption("")
-#     st.title("Zero-Shot Text Classifier")
+with c2:
+   st.caption("")     
+    st.title("Spill Emission Estimatpr")
 
 
 # We need to set up session state via st.session_state so that app interactions don't reset the app.
