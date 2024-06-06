94b366e5d3ad2ad340e0aaffee84b749b6b3d76c
blupho <82224096+blupho@users.noreply.github.com>
2024-06-06 18:13:36 -0500

    Update SEE.py

--- SEE.py
+++ SEE.py
@@ -5,11 +5,9 @@ import math
 st.set_page_config(
     layout="centered", page_title="Spill Emission Estimator", page_icon=":factory:"
 )
-
 #  create 2 columns to display the logo and the heading next to each other.
 c1, c2 = st.columns([0.3, 0.7])
 # spill boat will be displayed in the first column, on the left.
-
 with c1:
     st.image(
        "https://response.restoration.noaa.gov/sites/default/files/skimming_dwh_2010_noaa_520.jpg",
